84a5b9b78c2d94615ce6da72c83c1c88cffae7ce
CoffeeStraw <clarantonio98@gmail.com>
2019-02-11 18:05:39 +0100

    Added first section of examples, updated .gitignore file

---
+++ examples/1 - Basics/1 - Look into ASS values.py
@@ -0,0 +1,31 @@
+"""
+This script visualizes which ASS values you got from input ASS file.
+
+First of all you need to create an Ass object, which will help you to manage
+input/output. Once created, it will automatically extract all the informations
+from the input .ass file.
+
+For more info about the use of Ass class: 
+https://pyonfx.readthedocs.io/en/latest/reference/ass%20utility.html#pyonfx.ass_utility.Ass
+
+By executing this script, you'll discover how ASS contents,
+like video resolution, styles, lines etc. are stored into objects and lists.
+It's important to understand it, because these Python lists and objects
+are exactly the values you'll be working with the whole time to create KFX.
+
+Don't worry about the huge output, there are a lot of information
+even in a small input file like the one in this folder.
+
+You can find more info about each object used to represent the input .ass file here:
+https://pyonfx.readthedocs.io/en/latest/reference/ass%20utility.html
+"""
+from pyonfx import *
+
+io = Ass("in.ass")
+meta, styles, lines = io.get_data()
+
+print( meta )
+print("▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀")
+print( styles )
+print("▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀")
+print( lines )
---
+++ examples/1 - Basics/2 - Create the First Output.py
@@ -0,0 +1,30 @@
+"""
+This script creates your first dialog line in "Output.ass", which is
+the default name for the output that will contains our original dialog lines (commented) + our new generated lines.
+
+The magic function is write_line(), a class function of Ass
+which converts a dialog line of class Line back to text form and appends it to "Output.ass".
+For more info: https://pyonfx.readthedocs.io/en/latest/reference/ass%20utility.html#pyonfx.ass_utility.Ass.write_line
+
+To show the first manipulation, we take the first line of our input
+and print it back on the output changing only the text.
+
+It's not a good idea doing it this way because the original line text is overwritten and
+for future manipulations you will not be able to take the line's original values anymore
+without re parsing again the input file creating a new Ass object.
+
+Instead, you should always create a copy of line to save the original, we will see how in the following examples.
+
+At the end, you have to call save() class method to actually write your output.
+PyonFX will also automatically print how many lines you've written and the process duration.
+For more info: https://pyonfx.readthedocs.io/en/latest/reference/ass%20utility.html#pyonfx.ass_utility.Ass.save
+"""
+from pyonfx import *
+
+io = Ass("in.ass")
+meta, styles, lines = io.get_data()
+
+lines[0].text = "I am a new line!"
+io.write_line(lines[0])
+
+io.save()
