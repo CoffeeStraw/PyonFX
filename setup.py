b0b6780f6120c154cee51c7d8c5a524233a6a356
CoffeeStraw <clarantonio98@gmail.com>
2019-02-12 00:39:38 +0100

    Fixed a bug which caused missing modules error

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
+import os
+import re
 import setuptools
-from pyonfx import __version__
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    with open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = \'(.+)\'",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 setuptools.setup(
     name="pyonfx",
     url="https://github.com/CoffeeStraw/PyonFX",
     author="Antonio Strippoli",
     author_email="clarantonio98@gmail.com",
-    version=__version__,
+    version=find_version("pyonfx", "__init__.py"),
     license='GNU LGPL 3.0 or later',
     description="An easy way to do KFX and complex typesetting based on subtitle format ASS (Advanced Substation Alpha).",
     long_description=open('README.md').read(),
