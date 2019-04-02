d6063d6a42135a25ea4d71122e41b6f6ccc81dd0
CoffeeStraw <clarantonio98@gmail.com>
2019-04-03 00:44:33 +0200

    Minor doc update for the settings section

--- pyonfx/settings.py
+++ pyonfx/settings.py
@@ -22,8 +22,8 @@ class Settings:
 	}
 	"""
 	| A dictionary containing some easy to set parameters for mpv player.
-	| You can specify **video file path**, **video start** (https://mpv.io/manual/master/#options-start)
-	  and if you want to use **full screen** or not.
+	| You can specify **video file path** (str), **video start** (str) (https://mpv.io/manual/master/#options-start)
+	  and if you want to use **full screen** (bool) or not.
 	| If you don't specify anything, it will automatically assume as video file **meta.video**,
 	| as video start **"0"** and it will **not play in full screen**
 	"""
