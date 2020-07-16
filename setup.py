2d4560584924c546ad910a816d06da7442c17603
CoffeeStraw <clarantonio98@gmail.com>
2020-07-16 22:42:55 +0200

    Attempt to fix readthedocs doc compilation

--- setup.py
+++ setup.py
@@ -4,17 +4,19 @@ import setuptools
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+
 def read(*parts):
-    with open(os.path.join(here, *parts), 'r') as fp:
+    with open(os.path.join(here, *parts), "r") as fp:
         return fp.read()
+
 
 def find_version(*file_paths):
     version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = \'(.+)\'",
-                              version_file, re.M)
+    version_match = re.search(r"^__version__ = \'(.+)\'", version_file, re.M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
+
 
 setuptools.setup(
     name="pyonfx",
@@ -22,28 +24,21 @@ setuptools.setup(
     author="Antonio Strippoli",
     author_email="clarantonio98@gmail.com",
     version=find_version("pyonfx", "__init__.py"),
-    license='GNU LGPL 3.0 or later',
+    license="GNU LGPL 3.0 or later",
     description="An easy way to do KFX and complex typesetting based on subtitle format ASS (Advanced Substation Alpha).",
-    long_description=open('README.md', encoding='utf-8').read(),
-    packages=['pyonfx'],
+    long_description=open("README.md", encoding="utf-8").read(),
+    packages=["pyonfx"],
     install_requires=[
-        "pywin32; sys_platform == \"win32\"",
-        "pycairo; sys_platform == \"linux\"",
-        "PyGObject; sys_platform == \"linux\"",
+        'pywin32; sys_platform == "win32"',
+        'pycairo; sys_platform == "linux"',
+        'PyGObject; sys_platform == "linux"',
         "pyquaternion",
     ],
-    extras_require={
-        'dev': [
-            'pytest',
-            'pytest-check',
-            'sphinx_rtd_theme',
-            'sphinxcontrib-napoleon',
-        ]
-    },
+    extras_require={"dev": ["pytest", "pytest-check",]},
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
     ],
 )
