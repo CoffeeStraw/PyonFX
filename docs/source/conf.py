e3e9c0ddb408aa9fd8224d7cb3703a7d6598798b
CoffeeStraw <clarantonio98@gmail.com>
2021-02-21 01:36:07 +0100

    Removed type hints from documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ version = ""
 release = __version__
 
 # -- General configuration ---------------------------------------------------
+autodoc_typehints = "none"
 autodoc_member_order = "bysource"
 
 extensions = [
