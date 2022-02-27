6ea316610786baf08dacb26dc6c9568deccdbb39
Funami580 <63090225+Funami580@users.noreply.github.com>
2022-02-27 19:35:47 +0100

    Shape: add exact bounding box calculation

--- pyonfx/shape.py
+++ pyonfx/shape.py
@@ -326,10 +326,13 @@ class Shape:
         self.elements = transformed_elements
         return self
 
-    def bounding(self) -> tuple[float, float, float, float]:
+    def bounding(self, exact: bool = True) -> tuple[float, float, float, float]:
         """Calculates shape bounding box.
 
         **Tips:** *Using this you can get more precise information about a shape (width, height, position).*
+
+        Parameters:
+            exact (bool): Whether the calculation of the bounding box should be exact, which is more precise for bezier curves.
 
         Returns:
             A tuple (x0, y0, x1, y1) containing coordinates of the bounding box.
@@ -337,17 +340,122 @@ class Shape:
         Examples:
             ..  code-block:: python3
 
-                print("Left-top: %d %d\\nRight-bottom: %d %d" % ( Shape("m 10 5 l 25 5 25 42 10 42").bounding() ) )
+                print("Left-top: %d %d\\nRight-bottom: %d %d" % (Shape("m 10 5 l 25 5 25 42 10 42").bounding()))
+                print(Shape("m 313 312 b 254 287 482 38 277 212 l 436 269 b 378 388 461 671 260 481").bounding())
+                print(Shape("m 313 312 b 254 287 482 38 277 212 l 436 269 b 378 388 461 671 260 481").bounding(exact=False))
 
             >>> Left-top: 10 5
             >>> Right-bottom: 25 42
+            >>> (260.0, 150.67823683425252, 436.0, 544.871772934194)
+            >>> (254.0, 38.0, 482.0, 671.0)
         """
         all_points = [coord for element in self for coord in element.coordinates]
 
         if not all_points:
             return (0.0, 0.0, 0.0, 0.0)
 
+        if exact:
+            return self.__bounding_exact()
+
         return MultiPoint(all_points).bounds
+
+    def __bounding_exact(self) -> tuple[float, float, float, float]:
+        # From: https://stackoverflow.com/a/14429749
+        def get_bounds_of_curve(x0, y0, x1, y1, x2, y2, x3, y3):
+            tvalues = []
+
+            for i in range(2):
+                if i == 0:
+                    b = 6 * x0 - 12 * x1 + 6 * x2
+                    a = -3 * x0 + 9 * x1 - 9 * x2 + 3 * x3
+                    c = 3 * x1 - 3 * x0
+                else:
+                    b = 6 * y0 - 12 * y1 + 6 * y2
+                    a = -3 * y0 + 9 * y1 - 9 * y2 + 3 * y3
+                    c = 3 * y1 - 3 * y0
+
+                if abs(a) < 1e-12:  # Numerical robustness
+                    if abs(b) < 1e-12:  # Numerical robustness
+                        continue
+                    t = -c / b
+                    if 0 < t < 1:
+                        tvalues.append(t)
+                    continue
+                b2ac = b * b - 4 * c * a
+                if b2ac < 0:
+                    continue
+                sqrtb2ac = math.sqrt(b2ac)
+                t1 = (-b + sqrtb2ac) / (2 * a)
+                if 0 < t1 < 1:
+                    tvalues.append(t1)
+                t2 = (-b - sqrtb2ac) / (2 * a)
+                if 0 < t2 < 1:
+                    tvalues.append(t2)
+
+            x_min, y_min, x_max, y_max = math.inf, math.inf, -math.inf, -math.inf
+
+            for t in tvalues:
+                mt = 1 - t
+                x = (
+                    (mt * mt * mt * x0)
+                    + (3 * mt * mt * t * x1)
+                    + (3 * mt * t * t * x2)
+                    + (t * t * t * x3)
+                )
+                x_min, x_max = min(x_min, x), max(x_max, x)
+                y = (
+                    (mt * mt * mt * y0)
+                    + (3 * mt * mt * t * y1)
+                    + (3 * mt * t * t * y2)
+                    + (t * t * t * y3)
+                )
+                y_min, y_max = min(y_min, y), max(y_max, y)
+
+            x_min, x_max = min(x_min, x0), max(x_max, x0)
+            y_min, y_max = min(y_min, y0), max(y_max, y0)
+            x_min, x_max = min(x_min, x3), max(x_max, x3)
+            y_min, y_max = min(y_min, y3), max(y_max, y3)
+
+            if math.inf in (x_min, y_min) or -math.inf in (x_max, y_max):
+                raise ValueError("Invalid bezier curve")
+
+            return x_min, y_min, x_max, y_max
+
+        x_min, y_min, x_max, y_max = math.inf, math.inf, -math.inf, -math.inf
+
+        def update_min_max(x, y):
+            nonlocal x_min, y_min, x_max, y_max
+            x_min = min(x_min, x)
+            y_min = min(y_min, y)
+            x_max = max(x_max, x)
+            y_max = max(y_max, y)
+
+        def points_to_tuples(coordinates: list[Point]) -> list[tuple[float, float]]:
+            return [(coord.x, coord.y) for coord in coordinates]
+
+        cursor = (0, 0)
+        for element in self:
+            if element.command == "m":
+                cursor = points_to_tuples(element.coordinates)[0]
+            elif element.command == "l":
+                update_min_max(*cursor)
+                cursor = points_to_tuples(element.coordinates)[0]
+                update_min_max(*cursor)
+            elif element.command == "b":
+                coords = points_to_tuples(element.coordinates)
+                bounds = get_bounds_of_curve(*cursor, *[i for c in coords for i in c])
+                update_min_max(*bounds[:2])
+                update_min_max(*bounds[2:])
+                cursor = coords[2]
+            else:
+                raise NotImplementedError(
+                    f"Drawing command '{element.command}' not implemented"
+                )
+
+        if math.inf in (x_min, y_min) or -math.inf in (x_max, y_max):
+            raise ValueError("Invalid or empty shape")
+
+        return x_min, y_min, x_max, y_max
 
     def move(self, x: float | None = None, y: float | None = None) -> Shape:
         """Moves shape coordinates in given direction.
