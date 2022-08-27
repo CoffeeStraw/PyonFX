43aa8d251c5baf6addd18d74749c41f0a0693724
moi15moi <moi15moismokerlolilol@gmail.com>
2022-08-27 11:51:31 -0400

    Import Time and Timecode module in __init__.py

--- pyonfx/__init__.py
+++ pyonfx/__init__.py
@@ -2,7 +2,7 @@
 
 from .font_utility import Font
 from .ass_core import Ass, Meta, Style, Line, Word, Syllable, Char
-from .convert import Convert, ColorModel
+from .convert import Convert, ColorModel, Time, Timecode
 from .shape import Shape
 from .utils import Utils, FrameUtility, ColorUtility
 
