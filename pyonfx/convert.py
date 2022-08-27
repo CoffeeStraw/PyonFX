ce369c9130b01d625929bb1cd6440f4bfbb17578
moi15moi <moi15moismokerlolilol@gmail.com>
2022-08-27 10:09:54 -0400

    Convert ms to ass timestamp like aegisub

--- pyonfx/convert.py
+++ pyonfx/convert.py
@@ -63,6 +63,9 @@ class Convert:
         """
         # Milliseconds?
         if type(ass_ms) is int and ass_ms >= 0:
+            
+            # From https://github.com/Aegisub/Aegisub/blob/6f546951b4f004da16ce19ba638bf3eedefb9f31/libaegisub/include/libaegisub/ass/time.h#L32
+            ms = (ms + 5) - (ms + 5) % 10 
 
             return "{:d}:{:02d}:{:02d}.{:02d}".format(
                 math.floor(ass_ms / 3600000) % 10,
