468be984da7e3ea468673fee0f11d1ecf663e1f5
CoffeeStraw <clarantonio98@gmail.com>
2025-07-18 23:14:31 +0200

    Export Pixel in __init__

--- pyonfx/__init__.py
+++ pyonfx/__init__.py
@@ -3,5 +3,5 @@
 from .ass_core import Ass, Char, Line, Meta, Style, Syllable, Word
 from .convert import ColorModel, Convert
 from .font_utility import Font
-from .shape import Shape, ShapeElement
+from .shape import Shape, ShapeElement, Pixel
 from .utils import ColorUtility, FrameUtility, Utils
