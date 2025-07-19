62359824f1fae601f9ca7ba09e1496677d04f029
CoffeeStraw <clarantonio98@gmail.com>
2025-07-19 14:12:51 +0200

    Update modules copyright header

--- pyonfx/shape.py
+++ pyonfx/shape.py
@@ -1,5 +1,5 @@
 # PyonFX: An easy way to create KFX (Karaoke Effects) and complex typesetting using the ASS format (Advanced Substation Alpha).
-# Copyright (C) 2019 Antonio Strippoli (CoffeeStraw/YellowFlash)
+# Copyright (C) 2019-2025 Antonio Strippoli (CoffeeStraw/YellowFlash)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
